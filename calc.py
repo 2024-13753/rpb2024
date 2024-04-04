fae9fd3819fd3c8d79018fc19f0cafa76fc649a1
2024-13753 <165995381+2024-13753@users.noreply.github.com>
2024-04-04 23:21:10 +0900

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,22 @@
 def main():
+    print("Let's implement addition. Type two numbers for x and y.")
+
+    x = int(input("x > "))
+    y = int(input("y > "))
+    
+    print("%d + %d = %d" % (x, y, add(x, y)))    
+    
+def add(x,y):
+    return x+y
+
+if __name__ == "__main__":
+    main()
+
+def divide(x,y): 
+    if y == 0:
+        print("Error: cannot divide by zero.")
+    else:
+        return x/y
+
+if __name__ == "__main__":
+    main()
